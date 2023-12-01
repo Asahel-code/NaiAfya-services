cfdf5ffa18cb16c6130ba0987c1987b105a3cf53
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-12-01 09:20:08 +0300

    fix: Date filter bug fix

--- dashboard/src/utils/arrayOfDatesFilterValues.js
+++ dashboard/src/utils/arrayOfDatesFilterValues.js
@@ -1,11 +1,15 @@
 export const getArrayOfDates =() => {
     const currentDate = new Date();
+    const dayOfWeek = currentDate.getDay();
+    const currentWeek = new Date(currentDate);
 
-    const today = new Date(`${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)}-${currentDate.getDate()}`).toISOString();
+    currentWeek.setDate(currentDate.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1));
 
-    const startOfWeek = new Date(`${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)}-${currentDate.getDate() - currentDate.getDay()}`).toISOString();
+    const today = new Date(`${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`).toISOString();
 
-    const startOfMonth = new Date(`${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)}-01`).toISOString();
+    const startOfWeek = new Date(`${currentWeek.getFullYear()}-${(currentWeek.getMonth() + 1).toString().padStart(2, '0')}-${currentWeek.getDate().toString().padStart(2, '0')}`).toISOString();
+
+    const startOfMonth = new Date(`${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-01`).toISOString();
 
     const startOfYear = new Date(`${currentDate.getFullYear()}-01-01`).toISOString();
 
